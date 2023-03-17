28cdf7bd13dab02d27c12705d5c955e3b4955358
angkahokiads <128157461+angkahokiads@users.noreply.github.com>
2023-03-17 22:27:43 +0700

    Update hongkong.php

--- hongkong.php
+++ hongkong.php
@@ -1,13 +1,11 @@
 <table><tr><td class="tanggal" colspan="2">Live draw HK hari Kamis , Tanggal 16 Maret  2023</td></tr><tr>
 <td colspan='8' style='width:100%; height: auto; box-sizing: border-box; border: 1px solid white;'>
-<a href='https://mianoy.com/?ref=ld156165' target='_blank'><img style='width: 100%; box-sizing: border-box;' src='https://res.cloudinary.com/f0ustf0ur/image/upload/v1668608434/KPKTOGEL_2_iaxfxx.gif'></a>
 </td>
 </tr><tr height=40><td class="prize" colspan="1">PRIZE 1</td><td class="othernum" align="center"><a class="prize1" style = "color:#red;text-decoration:none;" href = "http://159.223.39.75/">
 861971
 
 </a></td></tr><tr>
 <td colspan='8' style='width:100%; height: auto; box-sizing: border-box; border: 1px solid white;'>
-<a href='https://sikecilxxx.com/link.php?member=ld156165' target='_blank'><img style='width: 100%; box-sizing: border-box;' src='https://res.cloudinary.com/f0ustf0ur/image/upload/v1662203722/HONDATOTO_vaxbwd.gif'></a>
 </td>
 </tr><tr height=40><td class="prize" colspan="1">PRIZE 2</td><td class="othernum" align="center">
 776798
@@ -16,7 +14,6 @@
     
 </td></tr><tr>
 <td colspan='8' style='width:100%; height: auto; box-sizing: border-box; border: 1px solid white;'>
-<a href='https://miatisong.com/?ref=ld156165' target='_blank'><img style='width: 100%; box-sizing: border-box;' src='https://res.cloudinary.com/f0ustf0ur/image/upload/v1668608437/OJKSLOT_2_ybjggq.gif'></a>
 </td>
 </tr><tr height=40><td class="prize" colspan="1">PRIZE 3</td><td class="othernum" align="center">
 082728
