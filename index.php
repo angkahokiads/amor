54032c0f12f70610a122000b7c980d913da98de7
angkahokiads <128157461+angkahokiads@users.noreply.github.com>
2023-03-18 11:17:18 +0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,27 +1,24 @@
-<!DOCTYPE html>
-<html lang="id-ID">
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
- <head>
+<html lang="id-ID"><head>
 <title>Live Draw HK - Live HK Pools - Live Result HK - Live Hongkong Pools</title>
-<meta name="description"  content="Live Draw HK - Live HK Pools - Live Result HK - Live Hongkong Pools Situs pengeluaran togel hongkong dan live draw hongkong dengan pengeluaran hk tercepat hari ini." />
-<meta property="og:type" content="object" />
-<meta property="og:title" content="Live Draw HK - Live HK Pools - Live Result HK - Live Hongkong Pools" />
-<meta property="og:description" content="Live Draw HK - Live HK Pools - Live Result HK - Live Hongkong Pools Situs pengeluaran togel hongkong dan live draw hongkong dengan pengeluaran hk tercepat hari ini." />
-<meta property="og:url" content="https://angkahokiads.github.io/amor/" />
-<meta property="og:site_name" content="Live Draw HK - Live HK Pools - Live Result HK - Live Hongkong Pools" />
+<meta name="description" content="Live Draw HK - Live HK Pools - Live Result HK - Live Hongkong Pools Situs pengeluaran togel hongkong dan live draw hongkong dengan pengeluaran hk tercepat hari ini.">
+<meta property="og:type" content="object">
+<meta property="og:title" content="Live Draw HK - Live HK Pools - Live Result HK - Live Hongkong Pools">
+<meta property="og:description" content="Live Draw HK - Live HK Pools - Live Result HK - Live Hongkong Pools Situs pengeluaran togel hongkong dan live draw hongkong dengan pengeluaran hk tercepat hari ini.">
+<meta property="og:url" content="http://159.223.39.75/">
+<meta property="og:site_name" content="Live Draw HK - Live HK Pools - Live Result HK - Live Hongkong Pools">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="google-site-verification" content="tA4C8kmhtlg4Fw5i0mAwfrKjGkyQYVZ-8__Xb_gRSew" />
-<link rel="stylesheet" href="css/style.css" />
-<link rel="icon" href="img/favicon.png" sizes="32x32" />
-<link rel="apple-touch-icon-precomposed" href="img/favicon.png" />
+<meta name="google-site-verification" content="tA4C8kmhtlg4Fw5i0mAwfrKjGkyQYVZ-8__Xb_gRSew">
+<link rel="stylesheet" href="css/style.css">
+<link rel="icon" href="img/favicon.png" sizes="32x32">
+<link rel="apple-touch-icon-precomposed" href="img/favicon.png">
 <link href="https://fonts.googleapis.com/css?family=Oswald&amp;display=swap" rel="stylesheet">
-<script src="../ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 <script>
  $(document).ready(function() {
      $(".hk").load("hongkong.php");
    var refreshId = setInterval(function() {
-      $(".hk").load('hongkong42bd.php?randval='+ Math.random());
+      $(".hk").load('hongkong.php?randval='+ Math.random());
    }, 20000);
    $.ajaxSetup({ cache: false });
 });
@@ -31,6 +28,7 @@
 <body>
 
 <div class="banner-float">
+
 </div>
 
 <div class="container">
@@ -44,7 +42,7 @@
 </div>
 
 <div class="menu">
-<a href="https://angkahokiads.github.io/amor/" title="Live Draw HK" target="_blank">LIVE DRAW HK</a>
+<a href="#" title="Live Draw HK" target="_blank">LIVE DRAW HK</a>
 </div>
 
 <div class="content">
@@ -61,7 +59,7 @@
 
 <p>Lihat secara langsung pemutaran Live Draw HK di situs ini pada Pukul 22:30 WIB - 23:00 WIB (hingga selesai).</p>
 
-<div class="hk"> <table class="tablelive">
+<div class="hk"><table class="tablelive">
 <?php
 // URL TARGET
 $url = 'http://159.223.39.75/hongkong.php?randval=';
@@ -80,8 +78,7 @@ $text1 = $second_step[0];
 //Tampilkan 
 echo $text1 = str_replace('http://159.223.39.75/hongkong.php?randval=', 'https://angkahokiads.github.io/amor/', $second_step[0]);
 ?>
-</table>
-</div></div>
+</table></div>
 
 <p>Kami tidak lupa untuk memberikan link di bawah ini yang mungkin bisa membantu anda menemukan live draw pada pasaran lain:</p>
 <ul class="out-link">
@@ -99,7 +96,7 @@ echo $text1 = str_replace('http://159.223.39.75/hongkong.php?randval=', 'https:/
 <li>HK Prize Live</li>
 <li>Live Draw Bullseye</li>
 <li>Live Draw China</li>
-<li><a href="http://hotelkampong.eu.org/" target="_blank" title="Paito SDY">Paito SDY</a></li>
+<li><a href="#" target="_blank" title="Live Draw SDY">Live Draw SDY</a></li>
 </ul>
 
 <h2 style="margin-top: 20px;"><a class="title-head" href="https://angkahokiads.github.io/amor/" title="Result Keluaran HK">Result Keluaran HK</a></h2>
@@ -124,13 +121,11 @@ $second_step = explode("</table>" , $first_step[1] );
 // Replace syntax </tbody> dengan </tbody></table>
 $text1 = $second_step[0];
 //Tampilkan 
-echo $text1 = str_replace('http://159.223.39.75/', 'https://angkahokiads.github.io/amor/', $second_step[0]);
+echo $text1 = str_replace('https://angkahokiads.github.io/amor/', 'https://angkahokiads.github.io/amor/', $second_step[0]);
 ?>
 </table>
 </div>
-
- 
- <p>Jika anda penasaran dan jago dalam melakukan prediksi yang tepat, maka kami telah memberikan beberapa bandar terbaik. Bandar togel yang kami berikan pada baaner di atas merupakan agen terpercaya dan telah berpengalaman di Indonesia. Selain aman dalam melakukan pemasangan angka kamu juga akan mendapatkan pelayanan terbaik. Kunjung link yang telah kami berikan diatas untuk mendapatkan info lebih lanjut.</p>
+<p>Jika anda penasaran dan jago dalam melakukan prediksi yang tepat, maka kami telah memberikan beberapa bandar terbaik. Bandar togel yang kami berikan pada baaner di atas merupakan agen terpercaya dan telah berpengalaman di Indonesia. Selain aman dalam melakukan pemasangan angka kamu juga akan mendapatkan pelayanan terbaik. Kunjung link yang telah kami berikan diatas untuk mendapatkan info lebih lanjut.</p>
 
 <p>Itulah informasi yang dapat kami sampaikan kepada anda, semoga kamu menang dan selalu mendapatkan Jackpot. <b>Live Draw HK</b> ini akan selalu memberikan kemudahan kepada anda yang ingin mencari keluaran nomor hongkong tercepat.</p>
 
@@ -141,5 +136,5 @@ echo $text1 = str_replace('http://159.223.39.75/', 'https://angkahokiads.github.
 
 </div>
 </div>
-</body>
-</html>
+
+</body></html>
